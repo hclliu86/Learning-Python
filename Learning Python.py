07ec48bbf18ef9daa238414b399fdf4fd877c90d
Huichun Liu <hclliu86@gmail.com>
2015-09-20 07:19:06 +0800

    check if caching github pwd succeed

--- Learning Python.py
+++ Learning Python.py
@@ -1,3 +1,4 @@
 print("1th change")
 print("2th change on github website!")
 print("3th adding branch-1")
+print("4th caching github password in git by 'git config --global credential.helper wincred'")
